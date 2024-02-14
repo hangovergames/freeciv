7e7fa485de0e1f57312837004407c526330236cc
Jaakko Heusala <jhh@hg.fi>
2024-02-14 17:29:42 +0200

    Added embed.go to make using images possible from Go game

---
+++ data/trident/embed.go
@@ -0,0 +1,39 @@
+package images
+
+import (
+	_ "embed"
+)
+
+var (
+
+	//go:embed cities.png
+	Cities_png []byte
+
+	//go:embed earth.png
+	Earth_png []byte
+
+	//go:embed explosions.png
+	Explosions_png []byte
+
+	//go:embed extra_units.png
+	ExtraUnits_png []byte
+
+	//go:embed fog.png
+	Fog_png []byte
+
+	//go:embed grid.png
+	Grid_png []byte
+
+	//go:embed roads.png
+	Roads_png []byte
+
+	//go:embed select.png
+	Select_png []byte
+
+	//go:embed tiles.png
+	Tiles_png []byte
+
+	//go:embed units.png
+	Units_png []byte
+
+)
